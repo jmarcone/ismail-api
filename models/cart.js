7bf8d3fab0b852995bd355ffb0a6a140ed9b405c
jmarcone <marconi.julian@gmail.com>
2023-05-11 09:41:53 +0200

    fix order

--- models/cart.js
+++ models/cart.js
@@ -2,7 +2,7 @@ const pool = require("../db/pg");
 
 const getCart = async () => {
     const { rows: cart } = await pool.query(
-        "SELECT cart.*, products.* FROM cart JOIN products ON products.id = cart.product_id"
+        "SELECT cart.*, products.* FROM cart JOIN products ON products.id = cart.product_id ORDER by cart.id"
     );
 
     return cart;
@@ -19,12 +19,12 @@ const addToCart = async (product_id, quantity = 1) => {
 
 
     if (rows.rowCount !== 0) {
-         await pool.query(
+        await pool.query(
             "UPDATE cart SET quantity = quantity + $1 WHERE product_id = $2",
             [quantity, product_id]
         );
     } else {
-         await pool.query(
+        await pool.query(
             "INSERT INTO  cart (product_id, quantity) VALUES ($1, $2) RETURNING *",
             [product_id, quantity]
         );
@@ -36,7 +36,7 @@ const addToCart = async (product_id, quantity = 1) => {
 const removeFromCart = async (product_id) => {
     const rows = await pool.query("DELETE FROM cart WHERE product_id = $1", [product_id]);
 
-    return true;
+    return await getCart();
 }
 module.exports = {
     getCart,
